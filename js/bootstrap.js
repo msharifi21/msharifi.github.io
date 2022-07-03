1ee498f1cbeb2030e8b9faea5e4b956c7cf7e175
msharifi21 <95086094+msharifi21@users.noreply.github.com>
2022-07-03 08:11:31 -0600

    Add files via upload

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4462,13 +4462,3 @@ new Vue({
 }
 
 });
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
